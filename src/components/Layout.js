4c697db8ca77ed7d60de2fffea44507febef893a
anand <anandvipul01@gmail.com>
2023-05-01 22:33:52 +0530

    context

---
+++ src/components/Layout.js
@@ -0,0 +1,25 @@
+import { Context } from "../context";
+import { Navbar } from "./Navbar";
+import { UploadForm } from "./UploadForm";
+import { useContext } from "react";
+function Layout({ children }) {
+  const { state, dispatch } = useContext(Context);
+  const toggle = (bool) => dispatch({ type: "collapse", payload: { bool } });
+  return (
+    <>
+      <Navbar />
+      <div className="container  mt-5">
+        <button
+          className="btn btn-success float-end"
+          onClick={() => toggle(!state.isCollapsed)}
+        >
+          {state.isCollapsed ? "Close" : "+ Add"}
+        </button>
+        <div className="clearfix mb-4"></div>
+        <UploadForm />
+        {children}
+      </div>
+    </>
+  );
+}
+export default Layout;
