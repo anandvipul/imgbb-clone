b293bf9a72ad0a3502e9af959a5560f004e1ab54
Vipul Anand <45119902+anandvipul@users.noreply.github.com>
2023-05-17 13:11:34 +0530

    reset

--- src/components/Card.js
+++
@@ -1,9 +0,0 @@
-export const Card = ({ path, title }) => {
-  return (
-    <div className="col mb-5 ">
-      <div className="card" style={{ width: "18rem" }}>
-        <img src={path} className="card-img-top" alt={title} />
-      </div>
-    </div>
-  );
-};
